417cd289f5dfb6b23abdd7bab7c3a49571132136
Augusto Gomes <augusto.silva@elaw.com.br>
2020-02-08 19:23:25 -0300

    Api Backend finalizada

--- semana10/backend/src/index.js
+++ semana10/backend/src/index.js
@@ -1,11 +1,14 @@
 const express = require('express');
 const mongoose = require('mongoose');
 const routes = require('./routes');
+
+
 const app = express();
 
 mongoose.connect('mongodb+srv://admboka:Boka1309@cluster0-ux7kj.mongodb.net/week10?retryWrites=true&w=majority', {
     useNewUrlParser: true,
-    useUnifiedTopology: true});
+    useUnifiedTopology: true
+});
 
 app.use(express.json());
 app.use(routes);
@@ -13,4 +16,3 @@ app.use(routes);
 
 
 app.listen(3333);
-
--- semana10/backend/src/routes.js
+++ semana10/backend/src/routes.js
@@ -1,9 +1,17 @@
-const {Router} = require('express');
+const {
+    Router
+} = require('express');
+const DevController = require('./controllers/DevController');
+const SearchController = require('./controllers/SearchController');
+
+
 
 const routes = Router();
 
-routes.post('/users', (request,response) => {
-    return response.json({message: 'Hello World'});
-});
+routes.get('/devs', DevController.index);
+routes.post('/devs', DevController.store);
+
+routes.get('/search', SearchController.index);
+
 
 module.exports = routes;
